12f10cf7228ff29dae8323a5ef37667ac71a2eae
sion.keanna@gmail.com <sion.keanna@gmail.com@46e249c7-ffe0-94fa-642c-4c1ffa36bdbc>
2012-09-30 06:10:41 +0000

    JUnits for testing the Person classes

---
+++ trunk/M4/src/edu/gatech/oad/antlab/person/PersonsTestJUnit.java
@@ -0,0 +1,46 @@
+package edu.gatech.oad.antlab.person;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * These JUnits are just here for testing M4.
+ * You don't have to use them, I just like to
+ * practice using them.
+ * @author Keanna Sion
+ * @version 1.0
+ */
+public class PersonsTestJUnit {
+
+	@Test
+	public void person1Test() {
+		Person1 thing = new Person1("Sara");
+		String ace = thing.toString("gtg123b");
+		assertTrue("Person1 fails to rotate", 
+				ace.equals("Sarag123bgt"));
+	}
+	
+	@Test
+	public void person2Test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void person3Test() {
+		Person3 thing = new Person3("Keanna");
+		String ace = thing.toString("gtg123b");
+		assertTrue("Person3 fails to reverse",
+				ace.equals("Keannab321gtg"));
+	}
+	
+	@Test
+	public void person4Test() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void person5Test() {
+		fail("Not yet implemented");
+	}
+}
