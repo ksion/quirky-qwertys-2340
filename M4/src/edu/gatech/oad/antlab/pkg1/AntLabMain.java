aa3688dc34709d914e967a8beac3d5a2a28b8fc1
ajalma08@gmail.com <ajalma08@gmail.com@46e249c7-ffe0-94fa-642c-4c1ffa36bdbc>
2012-10-01 17:00:53 +0000

    Added resources.jar class path to AntLabMain.

--- M4/src/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M4/src/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -64,7 +64,7 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  //Person2 replace P2 with your name
 		  //and ggg000x with your gt num
 		  Person2 p2 = new Person2("Annette Almonte");
-		  toPrint += p2.toString("902758110");
+		  toPrint += p2.toString("ajam3");
 		  //Person3 replace P3 with your name
 		  //and ggg000x with your gt num
 		  Person3 p3 = new Person3("Keanna");
