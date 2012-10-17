5fd08dd8c80106cd2823d195d63c9f3287adbb89
Haydenericksonband@gmail.com <Haydenericksonband@gmail.com@46e249c7-ffe0-94fa-642c-4c1ffa36bdbc>
2012-10-17 02:30:44 +0000

    Not completely done but still functional for the map.

--- trunk/SpaceTraderGame/src/model/Planet.java
+++ trunk/SpaceTraderGame/src/model/Planet.java
@@ -1,14 +1,20 @@
 package model;
 
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 import java.util.Random;
+
+import javax.imageio.ImageIO;
 
 public class Planet {
 	Random rand = new Random();
 	private String name;
+	private final int HEIGHT = 451, WIDTH = 801, SIZE = 30;
 	private int currentCondition, techLevel; 
 	private Point position; 
 	private TradeGood[] goods; 
+	private String[] planetPics = {"src/view/blueGasPlanet.png","src/view/desertPlanet.png","src/view/desolatePlanet.png","src/view/jupiterPlanet.png", "src/view/redMineralPlanet.png"};
 	// private Vendor[] availVend;
 	private String[] vowels = {"a","e","i","o","u","y"}, consonants = {"b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","z"};
 	
@@ -21,14 +27,14 @@ public class Planet {
 	}
 	public Planet(Point p){
 		this.position = p;
-		this.name = "Planet X";
+		makeName();
 		this.techLevel = rand.nextInt(8);
 		this.currentCondition = rand.nextInt(13);
 	}
 	
 	public Planet(){
-		this.position = new Point(rand.nextInt(151), rand.nextInt(101));
-		this.name = "Planet X";
+		this.position = new Point(rand.nextInt(WIDTH), rand.nextInt(HEIGHT));
+		makeName();
 		this.techLevel = rand.nextInt(8);
 		this.currentCondition = rand.nextInt(13);
 	}
@@ -65,9 +71,40 @@ public class Planet {
 		return currentCondition;
 	}
 	
+	public TradeGood[] getTradeGoods(){
+		return goods;
+	}
+	
 	public String toString(){
 		String info = "Name: " + name + "\nPosition: " + position + "\nCurrent Condition: " + currentCondition + "\nTech Level: " + techLevel;
 		return info;
+	}
+	
+	public void draw(Graphics g){
+		String planetName = planetPics[rand.nextInt(5)];
+		Image img = null;
+		try {
+			img = ImageIO.read(new File(planetName));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		int x = (int)position.getX() - 15;
+		int y = (int)position.getY() - 15;
+		if(x - 10 <= 0){
+			x = 0;
+		}
+		if (y - 10 < 0){
+			y = 0;
+		}
+		if(x + 40 >= WIDTH){
+			x = WIDTH - SIZE;
+		}
+		if(y + 40 >= HEIGHT){
+			x = HEIGHT - SIZE;
+		}
+		
+		g.drawImage(img, x, y, SIZE, SIZE, null);
 	}
 
 }
