93c78b731ca952d2aef98461d3cf742765321d3a
sion.keanna@gmail.com <sion.keanna@gmail.com@46e249c7-ffe0-94fa-642c-4c1ffa36bdbc>
2012-10-18 01:10:38 +0000

    changed interface to class and stuffs...

--- SpaceTraderGame/src/model/SolarSystem.java
+++ SpaceTraderGame/src/model/SolarSystem.java
@@ -1,18 +1,53 @@
 package model;
 import java.awt.Point;
+import java.util.ArrayList;
 import java.util.List;
 
 
-public interface SolarSystem {
+public class SolarSystem {
+	private Point location;
+	private String name;
+	private ArrayList<Planet> planets;
+	private int count = 0;
+	private int techLevel;
+	private int resources;
+	private double taxRate;
+	
+	public SolarSystem(String n, Point p, ArrayList<Planet> pnts, int tL, double tR, int rS ) {
+		this.name = n;
+		this.planets = pnts;
+		this.location = p;
+		this.techLevel = tL;
+		this.taxRate = tR;
+		this.resources = rS;
+		count = planets.size();
+	}
+	
+	public SolarSystem(String n, ArrayList<Planet> pnts) {
+		this(n, new Point(0, 0), pnts, 5, 0.07, 7);
+	}
+	
+	public SolarSystem() {
+		name = "Random System";
+		planets = new ArrayList<Planet>(3);
+	}
 	
 	/**
 	 * Adds a Planet to the Solar System's underlying
-	 * List collection. Returns a boolean: true if add
+	 * array. Returns a boolean: true if add
 	 * was successful, false if otherwise.
 	 * @param Planet object
 	 * @return boolean
 	 */
-	public boolean add(Planet object);
+	public boolean add(Planet object) {
+		boolean ace = false;
+		if (!planets.contains(object)) {
+			planets.add(object);
+			ace = true;
+			count++;
+		}
+		return ace;
+	}
 	
 	/**
 	 * Removes a Planet from the underlying List collection.
@@ -21,14 +56,23 @@ public interface SolarSystem {
 	 * @param Planet object
 	 * @return boolean
 	 */
-	public boolean remove(Planet object);
+	public boolean remove(Planet object) {
+		boolean ace = false;
+		int idx = planets.indexOf(object);
+		if (idx >= 0){
+			planets.remove(idx);
+		}
+		return ace;
+	}
 	
 	/**
 	 * Returns the planets within the Solar System
-	 * as an ordered collection. 
-	 * @return List - of objects of type Planet
+	 * as an ArrayList
+	 * @return ArrayList<Planet> - of type Planet
 	 */
-	public List<Planet> getSolarSystem();
+	public ArrayList<Planet> getSolarSystem() {
+		return planets;
+	}
 	
 	/**
 	 * Returns the location of the Solar System 
@@ -36,7 +80,9 @@ public interface SolarSystem {
 	 * creation.
 	 * @return Point
 	 */
-	public Point getLocation();
+	public Point getLocation() {
+		return location;
+	}
 	
 	/**
 	 * Returns an int that represents the 
@@ -45,7 +91,9 @@ public interface SolarSystem {
 	 * and price of goods sold within the S.System.
 	 * @return int
 	 */
-	public int getTechLevel();
+	public int getTechLevel() {
+		return techLevel;
+	}
 	
 	/**
 	 * Returns an int that represents the resource 
@@ -53,7 +101,9 @@ public interface SolarSystem {
 	 * influences the price and quality of trade goods.
 	 * @return int
 	 */
-	public int getResources();
+	public int getResources() {
+		return resources;
+	}
 	
 	/**
 	 * Returns an int that represents the type of 
@@ -63,7 +113,9 @@ public interface SolarSystem {
 	 * traveling.
 	 * @return int 
 	 */
-	 public int getPoliSystem();
+	 public int getPoliSystem() {
+		return 0;
+	}
 	
 	/**
 	 * Returns a double that must be accounted for in all
@@ -71,13 +123,16 @@ public interface SolarSystem {
 	 * price.
 	 * @return double
 	 */
-	public double getTaxRate();
+	public double getTaxRate() {
+		return taxRate;
+	}
 	
 	/**
 	 * Sets the tax rate of the Solar System.
 	 * @param double new tax rate
 	 */
-	public void setTaxRate(double taxRate);
+	public void setTaxRate(double taxRate) {
+	}
 	
 	/**
 	 * Returns an int that represents the type of encounter
@@ -89,5 +144,7 @@ public interface SolarSystem {
 	 * ship.
 	 * @return int
 	 */
-	public int genEncounter();
+	public int genEncounter() {
+		return 0;
+	}
 }
