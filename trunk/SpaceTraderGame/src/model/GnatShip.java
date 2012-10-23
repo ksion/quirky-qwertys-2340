5995584025c33e861d90f37a60305453a239624d
sion.keanna@gmail.com <sion.keanna@gmail.com@46e249c7-ffe0-94fa-642c-4c1ffa36bdbc>
2012-10-23 03:13:31 +0000

    turned private into protected and i'm not sure if another class can go in and use the static variable if it's protected...correct the visibility if i'm wrong i think i am though...not sure...for whole team's future use...

--- trunk/SpaceTraderGame/src/model/GnatShip.java
+++ trunk/SpaceTraderGame/src/model/GnatShip.java
@@ -8,12 +8,13 @@ package model;
 public class GnatShip extends Ship {
 	
 	/** Strength of the Ship's hull. */
-	private static int hullStrength; 
+	protected static int hullStrength; 
 	
 	public GnatShip(int hullStrength){
 		super(hullStrength);
 		name = "Gnat Ship";
 	}
+	
 	/* 
 	 * Methods that will be added to code later:
 	 * public Point travel(Planet p){return null;}
