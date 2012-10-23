968324fb907ea7de9e410f0ccc5092806a917a0d
sion.keanna@gmail.com <sion.keanna@gmail.com@46e249c7-ffe0-94fa-642c-4c1ffa36bdbc>
2012-10-23 03:11:53 +0000

    totally not finished PriceModel class...i'll work on it in the morning/tomorrow?

---
+++ SpaceTraderGame/src/model/PriceModel.java
@@ -0,0 +1,13 @@
+package model;
+import model.Planet;
+public class PriceModel {
+
+	
+	public static int computeCost(TradeGood tg, TradeState ts) {
+		double tax = Planet.getTaxRate();
+		
+		
+		return 0;
+	}
+
+}
