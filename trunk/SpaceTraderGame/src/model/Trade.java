d80b3da9a7aad16ca5dd5dcc03dd0efda2acb0b2
sion.keanna@gmail.com <sion.keanna@gmail.com@46e249c7-ffe0-94fa-642c-4c1ffa36bdbc>
2012-10-23 03:04:15 +0000

    Created a Trade class to hold enum variable...for Sara's future use...*insert smilely here*

---
+++ trunk/SpaceTraderGame/src/model/Trade.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Trade {
+
+	/** Represents the current state of transaction **/
+	//view TradeState enum thingy
+	public static final TradeState TradeState = null;
+
+}
