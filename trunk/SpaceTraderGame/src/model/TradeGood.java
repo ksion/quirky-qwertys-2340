6523f3d45bc43da5dc7b4dd60e820b92b9963ff9
sion.keanna@gmail.com <sion.keanna@gmail.com@46e249c7-ffe0-94fa-642c-4c1ffa36bdbc>
2012-10-23 01:16:23 +0000

    TradeGood class which will have all the elements necessary to compute the cost of the good through the PriceModel...I will create the txt file next...

--- trunk/SpaceTraderGame/src/model/TradeGood.java
+++ trunk/SpaceTraderGame/src/model/TradeGood.java
@@ -1,4 +1,37 @@
 package model;
+
 public class TradeGood {
-	private static int cost;
+	protected static int cost;
+	private String name;
+	private int bP;
+	private int ttp;
+	private int mtlp;
+	private int mtlu;
+	private int var;
+	private int minSpace;
+	private int maxSpace;
+	
+	public TradeGood(String name, int basePrice, 
+			int techLevel, int minTechLPro, 
+			int minTechLUse, int vari, 
+			int minPriceSpace, int maxPriceSpace) {
+		this.name = name;
+		bP = basePrice;
+		ttp = techLevel;
+		mtlp = minTechLPro;
+		mtlu = minTechLUse;
+		var = vari;
+		minSpace = minPriceSpace;
+		maxSpace = maxPriceSpace;
+		cost = PriceModel.computeCost(this, Trade.TradeState);
+	}
+
+	public String getName() {return name;}
+	public int getBasePrice() {return bP;}
+	public int getTechLevel() {return ttp;}
+	public int getMinTechPro() {return mtlp;}
+	public int getMinTechUse() {return mtlu;}
+	public int getVariance() {return var;}
+	public int getMinSpacePrice() {return minSpace;}
+	public int getMaxSpacePrice() {return maxSpace;}
 }
