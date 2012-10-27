29dafa5e447e5e9565bb92731de1e15b753e4bf3
sion.keanna@gmail.com <sion.keanna@gmail.com@46e249c7-ffe0-94fa-642c-4c1ffa36bdbc>
2012-10-27 23:50:16 +0000

    TradeGood class enum holds all the possible tradegoods (excluding equipment)...i probably won't do the txt file thing mainly because it'll be a lot of work...simply pass a tradegood enum into a method and all the work would be done...you can ask a tradeGood's cost and it will tell you...stuff like that...

--- trunk/SpaceTraderGame/src/model/TradeGood.java
+++ trunk/SpaceTraderGame/src/model/TradeGood.java
@@ -1,37 +1,48 @@
 package model;
 
-public class TradeGood {
-	protected static int cost;
-	private String name;
-	private int bP;
-	private int ttp;
+import java.util.Random;
+
+public enum TradeGood {
+	WATER(0, 0, 1, 45, 5, 5, 50),
+	FURS(0, 0, 0, 250, 10, 20, 300),
+	FOOD(1, 0, 1, 100, 3, 5, 120),
+	ORE(2, 2, 3, 350, 20, 15, 350),
+	GAMES(3, 1, 6, 250, -10, 10, 200),
+	FIREARMS(3, 1, 5, 1255, 35, 10, 1400),
+	MEDICINE(4, 1, 6, 550, -25, 5, 600),
+	NARCHOTICS(5, 0, 5, 3500, -125, 150, 2000),
+	ROBOTS(6, 4, 7, 5000, -150, 100, 3500);
+	
 	private int mtlp;
 	private int mtlu;
+	private int ttp;
+	private int bP;
 	private int var;
+	private int ipl;
 	private int minSpace;
-	private int maxSpace;
+	private Random gen = new Random();
 	
-	public TradeGood(String name, int basePrice, 
-			int techLevel, int minTechLPro, 
-			int minTechLUse, int vari, 
-			int minPriceSpace, int maxPriceSpace) {
-		this.name = name;
-		bP = basePrice;
-		ttp = techLevel;
+	TradeGood(int minTechLPro, int minTechLUse, int techLProd,
+			int basePrice, int incrPLevel, int vari, 
+			int minPriceSpace) {
 		mtlp = minTechLPro;
 		mtlu = minTechLUse;
+		ttp = techLProd;
+		bP = basePrice;
 		var = vari;
+		ipl = incrPLevel;
 		minSpace = minPriceSpace;
-		maxSpace = maxPriceSpace;
-		cost = PriceModel.computeCost(this, Trade.TradeState);
 	}
-
-	public String getName() {return name;}
-	public int getBasePrice() {return bP;}
-	public int getTechLevel() {return ttp;}
-	public int getMinTechPro() {return mtlp;}
-	public int getMinTechUse() {return mtlu;}
-	public int getVariance() {return var;}
-	public int getMinSpacePrice() {return minSpace;}
-	public int getMaxSpacePrice() {return maxSpace;}
+	
+	int cost(int currentTechLevel, boolean inSpace) {
+		int ace = -1;
+		int variance = gen.nextInt(var);
+		if (inSpace && this.mtlu < currentTechLevel) {			
+			ace = minSpace + ipl * (ttp - mtlp) + variance;
+		} else if (!inSpace && this.mtlu < currentTechLevel) {
+			ace = bP + ipl * (ttp - mtlp) + variance;
+		}
+		return ace;
+	}
 }
+
