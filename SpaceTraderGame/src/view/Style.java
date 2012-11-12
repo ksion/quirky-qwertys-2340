1ed5ad9d14e31dec99f26a18004aaca2e25c18d6
sarahub <sarahub@46e249c7-ffe0-94fa-642c-4c1ffa36bdbc>
2012-11-12 00:39:32 +0000

    merged with head

--- SpaceTraderGame/src/view/Style.java
+++ SpaceTraderGame/src/view/Style.java
@@ -5,18 +5,30 @@ import java.awt.Font;
 
 import javax.swing.JLabel;
 
+/**
+ * class to hold common styles used across app
+ * @author Quirky Qwertys
+ *
+ */
 public class Style {
 	public static final Font SPACEAGE_NORMAL = new Font("Space Age", 1, 14);
 	public static final Color SPACEAGE_COLOR = new Color(0, 255, 0);
 	public static final Font ARIAL_NORMAL = new Font("Arial", 1, 14);
 
-
+	/**
+	 * adds spaceage styling to a label
+	 * @return the label
+	 */
 	public static JLabel createNormalLabel() {
 		JLabel label = new JLabel();
 		label.setFont(Style.SPACEAGE_NORMAL);
 		label.setForeground(Style.SPACEAGE_COLOR);
 		return label;
 	}
+	/**
+	 * adds the green color to the label
+	 * @return the label
+	 */
 	public static JLabel createSkillLabel() {
 		JLabel label = new JLabel();
 		label.setForeground(Style.SPACEAGE_COLOR);
