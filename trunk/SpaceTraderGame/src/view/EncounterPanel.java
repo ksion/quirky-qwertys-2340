7fc445fc9c0ca6d3bef94caee793fb58431b47b0
ajalma08@gmail.com <ajalma08@gmail.com@46e249c7-ffe0-94fa-642c-4c1ffa36bdbc>
2012-11-12 00:16:12 +0000

    Made JOptionPanes in EncounterPanel to tell player whether the game is over/how many credits have been earned/lost after encounter ends.

--- trunk/SpaceTraderGame/src/view/EncounterPanel.java
+++ trunk/SpaceTraderGame/src/view/EncounterPanel.java
@@ -1,16 +1,21 @@
 package view;
 
 import net.miginfocom.swing.MigLayout;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 
 import model.Player;
 import model.Pirate;
 import javax.swing.JButton;
+
+import java.awt.Dimension;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 import java.util.Random;
+import java.awt.event.*;
 
 /**
  * Represents the window that pops up if a pirate encounter
@@ -19,7 +24,7 @@ import java.util.Random;
  * @author Quirky Qwertys
  * @version 1.0 11.11.12
  */
-public class EncounterPanel extends JPanel {
+public class EncounterPanel extends JFrame {
 
 	private Pirate pirate = new Pirate();
 	private Player player;
@@ -27,6 +32,8 @@ public class EncounterPanel extends JPanel {
 	private JLabel yourDamagePoints;
 	private JLabel pirateDamagePoints;
 	private JButton btnFlee;
+	private Random random = new Random();
+	private JPanel panel;
 	
 	/**
 	 * Create the panel.
@@ -34,42 +41,54 @@ public class EncounterPanel extends JPanel {
 	public EncounterPanel(Player player) {
 		this.player = player;
 		
-		setLayout(new MigLayout("", "[grow][][grow]", "[][][][][][grow][][][grow][]"));
+		panel = new JPanel();
+		panel.setLayout(new MigLayout("", "[grow][][grow]", "[][][][][][grow][][][][grow][]"));
 		
 		JLabel message = new JLabel("You've encountered a pirate! You may fight or flee the pirate." +
 									" If you attack, ");
-		add(message, "cell 0 0 3 1");
+		panel.add(message, "cell 0 0 3 1");
 		
 		JLabel message2 = new JLabel("keep in mind that ships can only take up to 100 points of damage " +
 										  " before");
-		add(message2, "cell 0 1 3 1");
+		panel.add(message2, "cell 0 1 3 1");
 		
 		JLabel message3 = new JLabel("they are destroyed. If you flee, you may lose some credits.");
-		add(message3, "cell 0 2 3 1");
+		panel.add(message3, "cell 0 2 3 1");
 		
-		JLabel yourDamage = new JLabel("Your ship's damage:");
-		add(yourDamage, "flowx,cell 0 4 3 1");
-		
-		JLabel pirateDamage = new JLabel("Pirate ship's damage:" );
-		add(pirateDamage, "cell 0 5 3 1");		
+		JLabel label = new JLabel("");
+		panel.add(label, "cell 0 3 3 1");
 		
 		yourDamagePoints = new JLabel("0");
-		add(yourDamagePoints, "cell 1 4");
+		panel.add(yourDamagePoints, "cell 1 5");
+		
+		JLabel yourDamage = new JLabel("Your ship's damage:");
+		panel.add(yourDamage, "flowx,cell 0 5");
 		
 		pirateDamagePoints = new JLabel("0");
-		add(pirateDamagePoints, "cell 1 5");
+		panel.add(pirateDamagePoints, "cell 1 6");
+		
+		JLabel pirateDamage = new JLabel("Pirate ship's damage:" );
+		panel.add(pirateDamage, "cell 0 6");		
 		
 		damageInfo = new JLabel("");
-		add(damageInfo, "cell 0 7 3 1");
+		panel.add(damageInfo, "cell 0 8 3 1");
 		
 		JButton btnAttack = new JButton("Attack");
 		btnAttack.addActionListener(new AttackListener());
-		add(btnAttack, "cell 0 9,alignx center,aligny center");
+		panel.add(btnAttack, "cell 0 10,alignx center,aligny center");
 		
 	    btnFlee = new JButton(" Flee ");
 		btnFlee.addActionListener(new FleeListener());
-		add(btnFlee, "cell 2 9,alignx center");
+		panel.add(btnFlee, "cell 2 10,alignx center");
 		
+		getContentPane().add(panel);
+		setTitle("Pirate Encounter");
+		
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		setPreferredSize(new Dimension(510, 300));
+		pack();
+		setVisible(true);
+
 	}
 	
 	/**
@@ -86,10 +105,9 @@ public class EncounterPanel extends JPanel {
 		 * pirate's ship. The pirate immediately attacks.
 		 */
 		public void actionPerformed(ActionEvent event){
-			
-			btnFlee.setEnabled(false);
-			
-			if (player.getShip().getDamageSustained() < 100){
+			int playerDamage = player.getShip().getDamageSustained();
+			int pirateDamage = pirate.getShip().getDamageSustained();
+			if (playerDamage < 100 && pirateDamage < 100){
 				int currDamage = pirate.getShip().getDamageSustained();
 				player.getShip().attack(pirate.getShip());
 				damageInfo.setText("You dealt " + Integer.toString(pirate.getShip().getDamageSustained() 
@@ -97,10 +115,17 @@ public class EncounterPanel extends JPanel {
 				pirateDamagePoints.setText(Integer.toString(pirate.getShip().getDamageSustained()));
 				pirate.attack(player.getShip());
 				yourDamagePoints.setText(Integer.toString(player.getShip().getDamageSustained()));
-			}
-			else{
-			    damageInfo.setText("Your ship has been destroyed and you've lost all items!");
-			}
+				if (player.getShip().getDamageSustained() >= 100){
+					JOptionPane.showMessageDialog(null, "Your ship has been destroyed and you've lost all items. GAME OVER!");
+					dispose();
+				}
+				else if (pirate.getShip().getDamageSustained() >= 100){
+					int money = random.nextInt((int) (player.getMoney() *.20)) ;
+					player.setMoney(money);
+					JOptionPane.showMessageDialog(null, "You just gained " + money + " credits.");
+					dispose();
+				}
+			}	
 		}
 	}
 	
@@ -113,13 +138,15 @@ public class EncounterPanel extends JPanel {
 		
 		/**
 		 * Ends the user's encounter with the pirate. The user
-		 * can lose up to 40% of his/her credits when they flee.
+		 * can lose up to 20% of his/her credits when t flee.
 		 */
 		public void actionPerformed(ActionEvent event){
-			Random random = new Random();
-			int money = random.nextInt((int) (player.getMoney() *.40)) ;
-			player.setMoney(-1 * money);
-			damageInfo.setText("You just lost " + money + " credits.");
+			if (player.getMoney() != 0){
+				int money = random.nextInt((int) (player.getMoney() *.20)) ;
+				player.setMoney(-1 * money);
+				JOptionPane.showMessageDialog(null, "You just lost " + money + " credits.");
+				dispose();
+			}
 		}
 	}
 }
