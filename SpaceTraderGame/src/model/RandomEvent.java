2cb645bd0db34ec6dbbe2c4f2e2ff3bc5b28e7cd
ajalma08@gmail.com <ajalma08@gmail.com@46e249c7-ffe0-94fa-642c-4c1ffa36bdbc>
2012-11-12 02:10:27 +0000

    The game officially creates a RandomEvent. The user may be able to travel with no encounters or may encounter a Pirate.

--- SpaceTraderGame/src/model/RandomEvent.java
+++ SpaceTraderGame/src/model/RandomEvent.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Random;
+import view.EncounterPanel;
 /**
  * Represents a random event in the Space Trader universe
  * which can consist of a pirate encounter, finding a good,
@@ -7,8 +9,17 @@ package model;
  * 
  * @author Quirky Qwertys
  */
-public enum RandomEvent {
-	PIRATE,
-	FINDITEM,
-	LOSEITEM,
+public class RandomEvent{
+	
+	private Random random = new Random();
+	
+	public RandomEvent(Player player){
+		int event = random.nextInt(2);
+		switch(event){
+			case 0:
+				break;
+			case 1:
+				new EncounterPanel(player);
+		}
+	}
 }
