841f7bd9f7ebbd409da24831680846f3dd5cb348
sion.keanna@gmail.com <sion.keanna@gmail.com@46e249c7-ffe0-94fa-642c-4c1ffa36bdbc>
2012-11-19 03:39:15 +0000

    mew...

--- trunk/SpaceTraderGame/src/edu/gatech/quirkyqwerties/spacetraders/model/TradableItem.java
+++ trunk/SpaceTraderGame/src/edu/gatech/quirkyqwerties/spacetraders/model/TradableItem.java
@@ -1,3 +1,9 @@
+// $codepro.audit.disable emptyMethod
+/**
+ * TradableItem.java
+ * @version 1.0
+ * copyright 2012
+ */
 package edu.gatech.quirkyqwerties.spacetraders.model;
 
 import flexjson.JSON;
@@ -11,15 +17,16 @@ import flexjson.JSON;
  */
 
 public class TradableItem {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+
+	/** The type of trade good */
 	private TradeGood type;
+
+	/** The qty of the good. */
 	private int qty;
+
+	/** The price of the good. */
 	private int price;
-	
-	
+
 	/**
 	 * Instantiates a tradeable item that will go into 
 	 * the inventory.
@@ -32,50 +39,68 @@ public class TradableItem {
 		this.qty = qty;
 		this.price = price;
 	}
-	
+
+	/**
+	 * Constructor for TradableItem.
+	 */
 	protected TradableItem() {
 	}
-/**
- * Gets the quantity of the trade good
- * @return the quantity
- */
+
+	/**
+	 * Gets the quantity of the trade good
+	 *
+	 * @return the quantity 
+	 */
 	public int getQty() {
 		return qty;
 	}
-/**
- * Sets the quantity of the trade good
- * @param qty the new quantity of the trade good
- */
+
+	/**
+	 * Sets the quantity of the trade good
+	 * @param qty the new quantity of the trade good
+	 */
 	public void setQty(int qty) {
 		this.qty = qty;
 	}
 
 	/**
 	 * Retrieves the type of trade good
-	 * @return the trade good
-	 */
+	 *
+	 * @return the trade good */
 	public TradeGood getType() {
 		return type;
 	}
 
 	/**
 	 * Gets the price of the trade good
-	 * @return the price of the good
-	 */
+	 *
+	 * @return the price of the good */
 	public int getPrice() {
 		return price;
 	}
-	
+
+	/**
+	 * Method setPrice.
+	 * @param price int
+	 */
 	protected void setPrice(int price) {
 		this.price = price;
 	}
-	
+
 	/**
 	 * Gets the name of the trade good
-	 * @return the name of the good
-	 */
+	 *
+	 * @return the name of the good */
 	@JSON(include=false)
 	public String getName(){
 		return type.getName();
-	}	
+	}
+
+	/**
+	 * Method toString.
+	 * @return String
+	 */
+	public String toString() {
+		return "[ " + type + ", " + qty + ", " + price + " ]";
+	}
 }
