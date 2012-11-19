e9a8b33dc9ba7499b46cfae314adc4ae64207fe1
sion.keanna@gmail.com <sion.keanna@gmail.com@46e249c7-ffe0-94fa-642c-4c1ffa36bdbc>
2012-11-19 04:45:07 +0000

    done...

--- SpaceTraderGame/src/edu/gatech/quirkyqwerties/spacetraders/view/TradeWindow.java
+++ SpaceTraderGame/src/edu/gatech/quirkyqwerties/spacetraders/view/TradeWindow.java
@@ -1,17 +1,18 @@
+// $codepro.audit.disable spaceAfterCasts, numericLiterals, appendString
+/**
+ * TradeWindow.java
+ * @version 1.0
+ * copyright 2012
+ */
 package edu.gatech.quirkyqwerties.spacetraders.view;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.List;
 
-import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
@@ -28,89 +29,101 @@ import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableCellRenderer;
 
+import net.miginfocom.swing.MigLayout;
 import edu.gatech.quirkyqwerties.spacetraders.controller.GameController;
 import edu.gatech.quirkyqwerties.spacetraders.model.Inventory;
 import edu.gatech.quirkyqwerties.spacetraders.model.Player;
 import edu.gatech.quirkyqwerties.spacetraders.model.TradableItem;
 
-import net.miginfocom.swing.MigLayout;
-
 /**
- * view of the trade window; can be trader/planet or trader/trader(future todo)
+ * view of the trade window; can be trader/planet or trader/trader(future todo).
+ * @version 1.0
  * @author QuirkyQwertys
- *
  */
-public class TradeWindow extends JPanel implements java.io.Serializable{
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	private JTable tableLeft;
-	private JTable tableRight;
-	private JLabel lblCredits;
+public class TradeWindow extends JPanel {
+
+	/** The table left. */
+	private final JTable tableLeft;
+
+	/** The table right. */
+	private final JTable tableRight;
+
+	/** The lbl credits. */
+	private final JLabel lblCredits;
+
+	/** The Constant BUY. */
 	private static final int BUY = 0;
+
+	/** The Constant SELL. */
 	private static final int SELL = 1;
-	private Image img;
-	private Color c = new Color(0, 255, 0);
-	private Font f = new Font("Space Age", 1, 12);
-	private TradeWindow t = this;
-	private Border tableBorder;
+
+	/** The t. */
+	private final TradeWindow tradeWin = this;
+
+	/** The table border. */
+	private final Border tableBorder;
 	
-	private GameController controller;
+	/** The controller. */
+	private final GameController controller;
 	
+	/** The player. */
 	private Player player;
+
+	/** The other inventory. */
 	private Inventory otherInventory;
 
 	/**
 	 * Create the panel.
+	 *
+	 * @param controller the controller
+	 * @throws IOException Signals that an I/O exception has occurred.
 	 */
-	public TradeWindow(GameController controller) throws IOException {
+	public TradeWindow(GameController controller) { 
+		// $codepro.audit.disable com.instantiations.assist.eclipse.analysis.audit.rule.effectivejava.constructorsOnlyInvokeFinalMethods
 		this.controller = controller;
-		
-		
+
 		this.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
 		setLayout(new BorderLayout(0, 0));
-		
-	
+
 		setOpaque(false);
 		
-		//img = ImageIO.read(new File("src/view/Space.jpg"));
-		//img = ImageIO.read(getClass().getResource("/edu/gatech/quirkyqwerties/spacetraders/view/starsBackground.jpeg"));
-		
-		
-		JPanel tablePanel = new JPanel();
-		tablePanel.setBackground(new Color(0x66,0x66,0x66,125));
-		tablePanel.setBorder(new LineBorder(new Color(0x5d,0xdf,0xfb,255), 2,true));
+		final JPanel tablePanel = new JPanel();
+		tablePanel.setBackground(new Color(0x66, 0x66, 0x66, 125));
+		tablePanel.setBorder(new LineBorder(new 
+				Color(0x5d, 0xdf, 0xfb, 255), 2, true));
 		//tablePanel.setOpaque(false);
 		add(tablePanel, BorderLayout.CENTER);
-		tablePanel.setLayout(new MigLayout("", "[grow][][grow]", "[][][grow][][][grow][]"));
+		tablePanel.setLayout(new MigLayout("", "[grow][][grow]", 
+				"[][][grow][][][grow][]"));
 		
-		JLabel lblMessage = new JLabel("Use the arrow buttons to buy/trade items from inventory.");
+		final JLabel lblMessage = new JLabel("Use the arrow buttons to " +
+				"buy/trade items from inventory.");
 		lblMessage.setForeground(Style.SPACEAGE_COLOR);
 		lblMessage.setFont(Style.SPACEAGE_NORMAL);
 		tablePanel.add(lblMessage, "cell 0 0 3 1");
 		
-		JLabel lblTraderInventory = new JLabel("Your Inventory");
+		final JLabel lblTraderInventory = new JLabel("Your Inventory");
 		lblTraderInventory.setForeground(Style.SPACEAGE_COLOR);
 		lblTraderInventory.setFont(Style.SPACEAGE_NORMAL);
 		lblTraderInventory.setHorizontalAlignment(SwingConstants.CENTER);
 		tablePanel.add(lblTraderInventory, "cell 0 1");
 		
-		JLabel lblOtherInventory = new JLabel("Vendor Inventory");
+		final JLabel lblOtherInventory = new JLabel("Vendor Inventory");
 		lblOtherInventory.setForeground(Style.SPACEAGE_COLOR);
 		lblOtherInventory.setFont(Style.SPACEAGE_NORMAL);
 		lblOtherInventory.setHorizontalAlignment(SwingConstants.CENTER);
 		tablePanel.add(lblOtherInventory, "cell 2 1");
 		
-		JButton btnTradeBuy = new JButton("<==");
+		final JButton btnTradeBuy = new JButton("<==");
 		btnTradeBuy.setEnabled(false);
 		
-		JButton btnTradeSell = new JButton("==>");
+		final JButton btnTradeSell = new JButton("==>");
 		btnTradeSell.setEnabled(false);
 		
 		tableLeft = new JTable();
-		tableLeft.getSelectionModel().addListSelectionListener(new TableListener(tableLeft, btnTradeSell));
-		JScrollPane scrollPaneLeft = new JScrollPane(tableLeft);
+		tableLeft.getSelectionModel().addListSelectionListener(new 
+				TableListener(tableLeft, btnTradeSell));
+		final JScrollPane scrollPaneLeft = new JScrollPane(tableLeft);
 		scrollPaneLeft.setOpaque(false);
 		scrollPaneLeft.setBorder(null);
 		scrollPaneLeft.getViewport().setOpaque(false);
@@ -119,7 +132,7 @@ public class TradeWindow extends JPanel implements java.io.Serializable{
 		
 		tableRight = new JTable();
 		
-		tableBorder = new LineBorder(new Color(0,255,0,255),1);
+		tableBorder = new LineBorder(new Color(0, 255, 0, 255), 1);
 		/*
 		tableRight.setOpaque(false);
 		((DefaultTableCellRenderer)tableRight.getDefaultRenderer(Object.class)).setForeground(new Color(0,255,0));
@@ -129,35 +142,33 @@ public class TradeWindow extends JPanel implements java.io.Serializable{
 		*/
 		styleTable(tableRight);
 		styleTable(tableLeft);
-		tableRight.getSelectionModel().addListSelectionListener(new TableListener(tableRight,btnTradeBuy));
-		JScrollPane scrollPane = new JScrollPane(tableRight);
+		tableRight.getSelectionModel().addListSelectionListener(new TableListener(tableRight, btnTradeBuy));
+		final JScrollPane scrollPane = new JScrollPane(tableRight);
 		scrollPane.getViewport().setOpaque(false);
 		scrollPane.getViewport().setBorder(null);
 		scrollPane.setBorder(null);
 		scrollPane.setOpaque(false);
 		tablePanel.add(scrollPane, "cell 2 2 1 4,grow");
-		
-		
-		
+
 		btnTradeSell.addActionListener(new ArrowListener(SELL));
 		tablePanel.add(btnTradeSell, "cell 1 3");
-		
-		
+
 		btnTradeBuy.addActionListener(new ArrowListener(BUY));
 		tablePanel.add(btnTradeBuy, "cell 1 4");
 		
-		JPanel buttonPanel = new JPanel();
+		final JPanel buttonPanel = new JPanel();
 		buttonPanel.setOpaque(false);
 		add(buttonPanel, BorderLayout.SOUTH);
 		buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.X_AXIS));
 		
-		JLabel lblCreditsAvailable = new JLabel("Credits Available: ");
+		final JLabel lblCreditsAvailable = new JLabel("Credits Available: ");
 		lblCreditsAvailable.setForeground(Style.SPACEAGE_COLOR);
 		lblCreditsAvailable.setFont(Style.SPACEAGE_NORMAL);
 		//buttonPanel.add(lblCreditsAvailable);
 		
 		lblCredits = new JLabel();
 		lblCredits.setForeground(Style.SPACEAGE_COLOR);
+		
 		lblCredits.setFont(Style.SPACEAGE_NORMAL);
 		//buttonPanel.add(lblCredits);
 		tablePanel.add(lblCredits, "cell 0 6");
@@ -166,16 +177,16 @@ public class TradeWindow extends JPanel implements java.io.Serializable{
 	
 		
 	}
-	
-	
+
 	/**
-	 * styles the table to make it transparent
-	 * @param table
+	 * styles the table to make it transparent.
+	 *
+	 * @param table the table
 	 */
 	public void styleTable(JTable table){
 		
 		table.setOpaque(false);
-		((DefaultTableCellRenderer)table.getDefaultRenderer(Object.class)).setForeground(new Color(0,255,0));
+		((DefaultTableCellRenderer)table.getDefaultRenderer(Object.class)).setForeground(new Color(0, 255, 0));
 		((DefaultTableCellRenderer)table.getDefaultRenderer(Object.class)).setOpaque(false);
 		table.setShowGrid(false);
 		table.setBorder(tableBorder);
@@ -184,7 +195,8 @@ public class TradeWindow extends JPanel implements java.io.Serializable{
 	}
 	
 /**
- * gets the right side inventory
+ * gets the right side inventory.
+ *
  * @return the right side inventory
  */
 	public Inventory getOtherInventory() {
@@ -192,7 +204,8 @@ public class TradeWindow extends JPanel implements java.io.Serializable{
 	}
 
 	/**
-	 * sets the inventory for the right side of the window
+	 * sets the inventory for the right side of the window.
+	 *
 	 * @param otherInventory right side inventory; could be planet or other trader
 	 */
 	public void setOtherInventory(Inventory otherInventory) {
@@ -201,7 +214,8 @@ public class TradeWindow extends JPanel implements java.io.Serializable{
 	}
 	
 	/**
-	 * gets the player's credits
+	 * gets the player's credits.
+	 *
 	 * @return player's money
 	 */
 	public int getCredits(){
@@ -209,39 +223,47 @@ public class TradeWindow extends JPanel implements java.io.Serializable{
 	}
 	
 	/**
-	 * sets the player
+	 * sets the player.
+	 *
 	 * @param p the player
 	 */
 	public void setPlayer(Player p){
 		player = p;
-		lblCredits.setText("Credits Available: "+Integer.toString(getCredits()));
+		lblCredits.setText("Credits Available: " + Integer.toString(getCredits()));
 		tableLeft.setModel(new InventoryTableModel(player.getCargo()));
 		repaint();
 		
 	}
 	
 	/**
-	 * model used by the tables in the gui
-	 * @author QuirkyQwertys
+	 * model used by the tables in the gui.
 	 *
+	 * @author QuirkyQwertys
 	 */
-	private class InventoryTableModel extends ListTableModel<TradableItem>{
-		private Inventory inventory;
+	private static class InventoryTableModel extends ListTableModel<TradableItem>{
+		
+		/** The inventory. */
+		private final Inventory inventory;
+
 		/**
-		 * model used by the table
+		 * model used by the table.
+		 *
 		 * @param inventory passing in the inventory to attach to the table
 		 */
-		public InventoryTableModel(Inventory inventory){
-			super(new String[] {"Name","Quantity", "Cost"},inventory.getGoods());
+		private InventoryTableModel(Inventory inventory){
+			super(new String[] {"Name", "Quantity", "Cost"}, inventory.getGoods());
 			this.inventory = inventory;
 		}
 
 		/**
-		 * gets the value in the list at that cell in the table
+		 * gets the value in the list at that cell in the table.
+		 *
+		 * @param row the row
+		 * @param col the col
 		 * @return the value in the cell
 		 */
 		public Object getValueAt(int row, int col) {
-			TradableItem item = get(row);
+			final TradableItem item = get(row);
 			switch(col){
 				case 0:return item.getName();
 				case 1: return item.getQty();
@@ -250,13 +272,16 @@ public class TradeWindow extends JPanel implements java.io.Serializable{
 			}
 		
 		}
+
 		/**
-		 * purchase action
+		 * purchase action.
+		 *
 		 * @param buy a tradable item
+		 * @param qty the qty
 		 * @return true if successful
 		 */
-		public boolean purchase( TradableItem buy, int qty ) {
-			boolean added = inventory.purchase(buy, qty);
+		public boolean canPurchase( TradableItem buy, int qty ) {
+			final boolean added = inventory.purchase(buy, qty);
 			if( added ) {
 				notifyChange();
 			}
@@ -264,64 +289,114 @@ public class TradeWindow extends JPanel implements java.io.Serializable{
 		}
 		
 		/**
-		 * sell action 
+		 * sell action.
+		 *
 		 * @param item a tradeable item
+		 * @param qty the qty
 		 * @return true if successful
 		 */
-		public boolean sold(TradableItem item, int qty) {
+		public boolean canSell(TradableItem item, int qty) {
 			notifyChange();
 			return true;
+		}
+		
+		/**
+		 * String Representation.
+		 * @return String
+		 */
+		public String toString() {
+			final StringBuffer res = new StringBuffer("[ ");
+			final List<TradableItem> ace = this.inventory.getGoods();
+			for (TradableItem x : ace) {
+				res.append(x + " ");
+			}
+			res.append("]");
+			return res.toString();
 		}
 	}
 	
 	/**
-	 * class to make sure an item in the table is selected
-	 * @author Quirky Qwertys
+	 * class to make sure an item in the table is selected.
 	 *
+	 * @author Quirky Qwertys
 	 */
-	private class TableListener implements ListSelectionListener{
-		JTable table;
-		JButton button;
-		TableListener(JTable table, JButton button) {
+	private static class TableListener implements ListSelectionListener{
+		
+		/** The table. */
+		private final JTable table;
+
+		/** The button. */
+		private final JButton button;
+
+		/**
+		 * Instantiates a new table listener.
+		 *
+		 * @param table the table
+		 * @param button the button
+		 */
+		private TableListener(JTable table, JButton button) {
 			    this.table = table;
 			    this.button = button;
 		}
+
 		/**
-		 * checks whether an item from the table is selected before enabling the buttons
+		 * checks whether an item from the table is selected before enabling the buttons.
+		 *
+		 * @param e the e
 		 */
 		public void valueChanged(ListSelectionEvent e) {
 				button.setEnabled(! (table.getSelectionModel().isSelectionEmpty()) );
 			
 		}
-					
+		
+		/**
+		 * String Representation
+		 * @return String
+		 */
+		public String toString() {
+			return null;
+		}
 		
 	}
+
 	/**
-	 * listener for the buy/sell buttons
-	 * @author Quirky Qwertys
+	 * listener for the buy/sell buttons.
 	 *
+	 * @author Quirky Qwertys
 	 */
 	private class ArrowListener implements ActionListener{
-		private int transaction;
+		
+		/** The transaction. */
+		private final int transaction;
+
 		/**
-		 * constructor for the listener which takes in one of the buy/sell buttons
-		 * @param direction buy/sell
+		 * constructor for the listener which takes in one of the buy/sell 
+		 * buttons.
+		 *
+		 * @param transaction the transaction
 		 */
-		public ArrowListener(int transaction){
+		private ArrowListener(int transaction){
 			this.transaction = transaction;
 		
 	    }
+
 		/**
-		 * action performed based on buy/sell; reloads the table when items move out
+		 * action performed based on buy/sell; reloads the table when items 
+		 * move out.
+		 *
+		 * @param e the e
 		 */
 		public void actionPerformed(ActionEvent e){
+			final Integer initialSelect = 0;
+			
 			if (transaction == BUY) {
 				//buying an item
-				TradableItem toBuy = getOtherInventory().getGoods().get(tableRight.getSelectedRow());
+				final TradableItem toBuy = getOtherInventory().getGoods().get(
+						tableRight.getSelectedRow());
 				int cost = toBuy.getPrice();
-				int qty = toBuy.getQty();
+				final int qty = toBuy.getQty();
 			
-				Object[] nums = new Object[qty + 1];
+				final Object[] nums = new Object[qty + 1];
 				for (int i = 0; i < qty + 1; i++) {
 					nums[i] = new Integer(i);
 				}
@@ -329,49 +404,63 @@ public class TradeWindow extends JPanel implements java.io.Serializable{
 				//need to find a way to set the new price you bought it at in the left
 				//table...the price stays the same for some reason... --> Keanna
 				
-				int qtyWant = (Integer) JOptionPane.showInputDialog(t, "How many to buy ?", 
-						"Quantity Buy", JOptionPane.PLAIN_MESSAGE, null, nums, new Integer(0));
+				final int qtyWant = (Integer) JOptionPane.showInputDialog(tradeWin, "How many to buy ?", 
+						"Quantity Buy", JOptionPane.PLAIN_MESSAGE, null, nums, initialSelect);
 				
-				InventoryTableModel model = (InventoryTableModel)tableLeft.getModel();
-				if (qty > 0 && cost < getCredits() && model.purchase(toBuy, qtyWant)) {
+				final InventoryTableModel model = (InventoryTableModel)tableLeft.getModel();
+				if (qty > 0 && cost < getCredits() && model.canPurchase(toBuy, qtyWant)) {
 					player.addMoney(-cost * qtyWant);
-					lblCredits.setText("Credits Available: "+Integer.toString(getCredits()));
+					lblCredits.setText("Credits Available: " + Integer.toString(getCredits()));
 					
-					InventoryTableModel soldModel = (InventoryTableModel)tableRight.getModel();
-					soldModel.sold(toBuy, qtyWant);
+					final InventoryTableModel soldModel = (InventoryTableModel)tableRight.getModel();
+					soldModel.canSell(toBuy, qtyWant);
 					repaint();
 				}
 			} else {
 				//player selling an item
-				TradableItem toSell = player.getCargo().getGoods().get(tableLeft.getSelectedRow());
+				final TradableItem toSell = player.getCargo().getGoods().get(tableLeft.getSelectedRow());
 				int cost = toSell.getPrice();
 				//Sell at the price they are selling --> Keanna
 				if (otherInventory.findItem(toSell) != null) {
-					TradableItem thing = otherInventory.findItem(toSell);
-					if (cost < thing.getPrice())
+					final TradableItem thing = otherInventory.findItem(toSell);
+					if (cost < thing.getPrice()) {
 						cost = thing.getPrice();
+					}
 				}
 				
-				int qty = toSell.getQty();
+				final int qty = toSell.getQty();
 			
-				Object[] nums = new Object[qty + 1];
+				final Object[] nums = new Object[qty + 1];
 				for (int i = 0; i < qty + 1; i++) {
 					nums[i] = new Integer(i);
 				}
 				
-				Integer qtyWant = (Integer)JOptionPane.showInputDialog(t, "How many to sell ?", 
-						"Quantity Sell", JOptionPane.PLAIN_MESSAGE, null, nums, new Integer(0));
+				final Integer qtyWant = (Integer)JOptionPane.showInputDialog(tradeWin, 
+						"How many to sell ?", 
+						"Quantity Sell", JOptionPane.PLAIN_MESSAGE, null, 
+						nums, initialSelect);
 				if( qtyWant != null ) {
-					InventoryTableModel model = (InventoryTableModel)tableLeft.getModel();
-					if (model.sold(toSell, qtyWant) == true){
+					final InventoryTableModel model = 
+							(InventoryTableModel)tableLeft.getModel();
+					if (model.canSell(toSell, qtyWant)){
 						player.addMoney(cost * qtyWant);
-						lblCredits.setText("Credits Available: "+Integer.toString(getCredits()));				
-						InventoryTableModel buyModel = (InventoryTableModel)tableRight.getModel();
-						buyModel.purchase(toSell, qtyWant);
+						lblCredits.setText("Credits Available: " + 
+								Integer.toString(getCredits()));	 // $codepro.audit.disable whiteSpaceUsage
+						final InventoryTableModel buyModel = 
+								(InventoryTableModel)tableRight.getModel();
+						buyModel.canPurchase(toSell, qtyWant);
 						repaint();
 					}
 				}
 			}
+		}
+		
+		/**
+		 * String Representation
+		 * @return String
+		 */
+		public String toString() {
+			return null;
 		}
     }
 	
